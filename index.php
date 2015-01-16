7b97260c55589d3e76be7308dcd6454d9989befe
Marike Meijer <marikemeijer@googlemail.com>
2015-01-16 13:50:02 +0100

    Impressum, help,
    update impressum with images, update help with img, add some designs for
    devices on index.php, change some styles

--- index.php
+++ index.php
@@ -12,24 +12,34 @@
 <html class="no-js" lang="en" >
 
 <head>
-    <meta charset="utf-8">
-    <!-- If you delete this meta tag World War Z will become a reality -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Marike Meijer">
-    <title>Geocomment</title>
+<meta charset="utf-8">
+<!-- If you delete this meta tag World War Z will become a reality -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="author" content="Marike Meijer">
+<title>Geocomment</title>
+<!-- Import JQUERY framework and css -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js"></script>
+<link rel="stylesheet" type="text/css" media="all"
+ href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/smoothness/jquery-ui.css"    />
 
-    <!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/foundation.css">
-    <!-- Karte-->
-    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
-    <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
-    <script src='https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.js'></script>
-    <link href='https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.css' rel='stylesheet' />
-    <link rel="stylesheet" href="css/mapstyle.css" />
-     <!-- If you are using the gem version, you need this only -->
-    <link rel="stylesheet" href="css/app.css">
-    <script src="js/vendor/modernizr.js"></script>
+ <!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
+ <link rel="stylesheet" href="css/normalize.css">
+ <link rel="stylesheet" href="css/foundation.css">
+ <!-- Karte-->
+ <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
+ <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+ <!-- Bring in the leaflet KML plugin -->
+ <!--<script src="leaflet-plugins/layer/vector/KML.js"></script>  
+ <script src='//api.tiles.mapbox.com/mapbox.js/plugins/leaflet-omnivore/v0.2.0/leaflet-omnivore.min.js'></script>   
+  -->
+ <script src='https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.js'></script>
+ <link href='https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.css' rel='stylesheet' />
+ <link rel="stylesheet" href="css/mapstyle.css" />
+ <!-- If you are using the gem version, you need this only -->
+ <link rel="stylesheet" href="css/app.css">
+ <script src="js/vendor/modernizr.js"></script>
+
   
  </head>
 
@@ -40,7 +50,8 @@
 
 
 <!--Topbar for Navigation by Marike -->
-<nav class="top-bar" data-topbar role="navigation">
+<div class="fixed">
+<nav class="top-bar"  data-topbar role="navigation" >
   <ul class="title-area" >
     <li class="name">
       <h1><a href="index.php">Geocomment</a></h1>
@@ -49,7 +60,7 @@
    
      <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
     <li class="toggle-topbar" id="menu-icon" ><a href='#'><span>Menu</span>
-       <img src="menu.png" > </a>
+       <img src="img/fi-info.svg" > </a>
        
     
          
@@ -61,19 +72,16 @@
     <!-- Middle Nav Section -->
     <ul class="middle">
                 
-         
-           
-          
-   
+     
 
         <!-- Start Login Formular--------------------------- -->
        <?php 
 
         // Only show form to unloggedin visitors
         if(IsLoggedIn() == true) {
-                echo file_get_contents("form_logout.php");
+            echo file_get_contents("form_logout.php");   
         } else {
-                echo file_get_contents("form_login.php");
+            echo file_get_contents("form_login.php");
         }
        ?>
 
@@ -82,27 +90,29 @@
       
      
 
-    <!--Registration//Sign in by Marike, Formular by Tobias-->
 
     <!-- <li><a href="#" data-reveal-id="sign-in">Sign In</a></li>-->
     
      <div id="sign-in" class="reveal-modal" data-reveal>
-         <h4> Sign In</h4>
+        
 
-         <script src="signin.js"></script>
+         <script src="js/signin.js"></script>
 
         <!-- Start Register Formular--------------------------- -->
           <?php 
-
-          // Only show form to unloggedin visitors
-          if(IsLoggedIn() == true) {
-                  echo "Already registered.";
-          } else {
-                  echo file_get_contents("form_register.php");
-          }
+            // Only show form to unloggedin visitors
+            if(IsLoggedIn() == true) {
+                echo "Already registered.";
+            } else {
+                echo requireToVar("form_register.php");
+            }
           ?>
 
           <!--------------------- End -->
+         
+           
+          
+   
 
     </div>
   </ul>
@@ -113,11 +123,11 @@
        <li class="has-dropdown">
         <a href="#">Menu</a>
         <ul class="dropdown">
-          <li><a href="#">Show all Comments</a></li>
+          <!--<li><a href="#">Show all Comments</a></li>
           <li><a href="#">Filter</a></li>
           <li><a href="#">New Comment</a></li>
-          <li><a href="#">Advanced Search</a></li>
-          <li><a href="Help.html">Help</a></li>
+          <li><a href="#">Advanced Search</a></li>-->
+          <li><a href="help.html">Help</a></li>
           <li><a href="impressum.html">Impressum</a></li>
 
         </ul>
@@ -147,12 +157,12 @@
        <strong class="show-for-medium-up">
        <li class="has-dropdown ">
 
-       <img src="info-icon-32.png" width="36px" height="36px" bottom="1px">
+       <img src="img/info-icon-32.png" width="36px" height="36px" bottom="1px">
       
        
-      <ul class="dropdown">
-          <li><a href="Help.html">Help</a></li>
-          <li><a href="Impressum.html">Impressum</a></li>
+       <ul class="dropdown">
+          <li><a href="help.html">Help</a></li>
+          <li><a href="impressum.html">Impressum</a></li>
        
         
 
@@ -163,6 +173,7 @@
     
   </section>
 </nav>
+</div>
 
 
 
@@ -177,30 +188,44 @@
 <div class="rows">
 
      
-  <div id="map">
+  <div id="map" >
    
-   
+   <link rel="stylesheet" href="css/mapstyle.css">
       <!--javascript for mapfunctions-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js "></script>
-    <script>
+
+       <script>
         // Generate Markers
         $(document).ready(function(){
-            <?php
-                $res=GetAllFirstComments();
-                for($i=0; $i < count($res); $i++) {
-                    $answers_num=count(GetCommentThread($res[$i]["id"]));
-                    echo "CreateMarker(".$res[$i]["positionx"]." , ".$res[$i]["positiony"]." , '"
-                            .$res[$i]["title"]."<br>".$res[$i]["content"]."<br>Comments: $answers_num' , '".$res[$i]["id"]."'); "; // Marker txt
+            ShowMarkers_all();
+            
+            <?php 
+                if($_GET['comment']!="") { // Permalink to comment
+                    echo 'ShowComment('.GetCommentIdByLink($_GET['comment']).");";
+                } elseif($_GET['tag']!="") { // Permalink to tags
+                    echo "ShowCommentsToTag('".$_GET['tag']."');";
+                } elseif($_GET['user']!="") { // Permalink to tags
+                    echo "ShowUserProfile('".$_GET['user']."');";
                 }
-             ?>
+            ?>
         });
     </script>  
     
-    <!--javascript for map function-->
-    <script src="map.js"></script>
- 
+  
+    
+  
+   
+    <script src="js/map.js"></script>
+
+
+    <!--
+    <script type="text/javascript" src="urlparser.js"></script>
+  -->
+
 
     }
+
+  
+
  
   
   </div>
@@ -211,113 +236,206 @@
 <!--map and comments part by marike-->
 
   <div class="small-6 medium-6 large-6 columns">
-  <!--map -->
+<!--map -->
  
   </div>
 
   <div class="small-5 hide-for-small medium-5 hide-for-medium  large-5 columns" id="backgroundright" >
       <dl class="tabs hide-for-small" data-tab >
-          <dd> <a  href="#panel1" id="secondStop">Home</a></dd>
-          <dd class="active"> <a href="#panel2" id="thirdStop">Comments  </a></dd>
-          <dd> <a  href="#panel3" id="stop4">Filter</a></dd>
+          <dd> <a  href="#panel1" onclick="ShowMarker_all()" id="secondStop">Create Comment</a></dd>
+          <dd class="active"> <a href="#panel2" onclick="ShowMarkers_all()" id="thirdStop">Show Comments  </a></dd>
+          <dd> <a  href="#panel3" id="stop4">Search</a></dd>
       </dl>
       
     <div class="tabs-content">
-      <div id="panel1" class="content" >
+    <div id="panel1" class="content" >
         <p> Welcome to Geocomment </p>
+
+        <a href='#' class='active' id='temperature'></a>
+        <a href='#' class='active' id='precipitation'></a>
          
 
 
-<div class="large-12 columns" >
+    <div class="large-12 columns" >
 
   <!--
   <a href="comment.html" class="button round" id="butcom1">New Comment</a>
-  -->
+  <button class="button" data-dropdown="newcom"  id="newcom2" aria-controls="newcom">New Comment</button>
+    -->
 
-<button class="button" data-dropdown="newcom"  id="newcom2" aria-controls="newcom">New Comment</button>
+    <script src="js/createcomment.js"></script>
 
-
-
-<script src="createcomment.js"></script>
-
- <!-- Start Create Comment Formular--------------------------- -->
-<?php 
+    <!-- Start Create Comment Formular--------------------------- -->
+    <?php 
 
     include "form_insertcomment.php";
 
-?>
-<!--------------------- End -->
-
-
-        
-    
-    </div>
-		  
-		  
-         
+    ?>
+    <!--------------------- End -->
        
-  </div>
-        <div class="content active"  id="panel2">
-<form name="Showcomments" action="http://giv-geosoft2d.uni-muenster.de" method="get">
-<fieldset>
-	<legend>Comments so far:</legend>
+   </div> 
+   </div> 
 
+   <!-- TAB COMMENTS-->
+    <div class="content active"  id="panel2">
 
-</fieldset>
-</form>
+        <form name="Showcomments" action="http://giv-geosoft2d.uni-muenster.de" method="get">
+        <fieldset>
+	      <legend>Last comments</legend>
+
+             <?php
+                // Display the 5 newest comments
+                 $res=GetAllFirstComments();
+                 for($i=0; $i < Min(5,count($res)); $i++)  {
+                        $html=$html.
+                                "<a href='#' onclick='ShowPosition(".$res[$i]["positionx"].",".$res[$i]["positiony"].
+                                      ")' > Goto </a> <br>";
+                        $html=$html.
+                                "<a href='#' onclick='ShowComment(".$res[$i]["id"].")' > Show Thread </a> <br>";
+                        $html=$html.GetUsernameFormatted($res[$i]["user_id"]) ."<br>";
+                        $html=$html.$res[$i]["timecreated"]."<br>";
+                        $html=$html.$res[$i]["title"]."<br>";
+                        $html=$html.$res[$i]["content"]."<br>";
+                        $html=$html."<a href='/final/index.php?comment=".$res[$i]["permalink"]."'> Permalink</a>". "<br>";
+                        $html=$html.TagsIdsToName($res[$i]["tags_ids"])."<br>";
+                        $html=$html."<br><br><br>";
+                 }
+
+                 echo $html;
+                ?>
+
+        </fieldset>
+        </form>
   
-        </div>
+        </div> 
 
-
+      <!--TAB FILTER-->
        <div class="content" id="panel3">
-	
-      <!--  <form name="Filter" action="http://de.selfhtml.org/cgi-bin/formview.pl" method="post" onsubmit="return chkFormular2()">
-          
-        -->
-            <h4>Filter</h4>
 
-            <p> Year </p>
-          <div class="range-slider radius" data-slider data-options="start:1960 ; end:2015;">
-			  <span class="range-slider-handle" role="slider" tabindex="0"></span>
-			  <span class="range-slider-active-segment"></span>
-			  <input type="hidden">
-		  </div>
+            <!-- Start Create Comment Formular--------------------------- -->
+        <?php 
 
-		   <p> Radius in km </p>
-		 <div class="range-slider" data-slider data-options="step: 10;">
-			  <span class="range-slider-handle" role="slider" tabindex="0"></span>
-			  <span class="range-slider-active-segment"></span>
-			  <input type="hidden">
-		</div> 
-			<div class="switch round tiny">
-			  <input id="exampleRadioSwitch3" type="radio" name="testGroup">
-			  <label for="exampleRadioSwitch3"></label>
-			</div>
-		</div>
+           include "form_filter.php";
 
+        ?>
+        <!--------------------- End -->
+	        
        
+        </div> 
        
-
-        </div>
-       
-
+     
         
-        
-        
+      
+      <!--SHOW COMMENTS Part--> 
         
         <div class="content" id="panel_showcomment">
-	  <div id="showncomments"> </div>
+	       <div id="showncomments"> 
+         </div>
         </div>
 
       </div>
             
-     
 
-    </div>
-  </div>
-  </div>
   
-</div>
+
+</div> 
+</div> 
+
+  <div class="small-12 medium-12 columns hide-for-large" id="bottomsmart">
+  <style type="text/css">
+  #bottomsmart{
+    position: absolute;
+    top:60%;
+
+
+
+  }
+  </style>
+    
+
+    <!--Devices View-->
+
+         <strong class="hide-for-large-up">
+
+
+<!-- Also works with <dl>'s and <dt>'s in place of <ul>'s and <li>'s. --> 
+
+<dl class="accordion" data-accordion>
+  <dd class="accordion-navigation">
+    <a href="#panel1b">Create comment</a>
+    <div id="panel1b" class="content active">
+      <script src="js/createcomment.js"></script>
+
+    <!-- Start Create Comment Formular--------------------------- -->
+    <?php 
+
+    include "form_insertcomment.php";
+
+    ?>
+    <!--------------------- End -->
+    </div>
+  </dd>
+  <dd class="accordion-navigation">
+    <a href="#panel2b">Last Comments</a>
+    <div id="panel2b" class="content">
+      <form name="Showcomments" action="http://giv-geosoft2d.uni-muenster.de" method="get">
+        <fieldset>
+        <legend>Last comments</legend>
+
+             <?php
+                // Display the 5 newest comments
+                 $res=GetAllFirstComments();
+                 for($i=0; $i < Min(5,count($res)); $i++)  {
+                        $html=$html.
+                                "<a href='#' onclick='ShowPosition(".$res[$i]["positionx"].",".$res[$i]["positiony"].
+                                      ")' > Goto </a> <br>";
+                        $html=$html.
+                                "<a href='#' onclick='ShowComment(".$res[$i]["id"].")' > Show Thread </a> <br>";
+                        $html=$html.GetUsernameFormatted($res[$i]["user_id"]) ."<br>";
+                        $html=$html.$res[$i]["timecreated"]."<br>";
+                        $html=$html.$res[$i]["title"]."<br>";
+                        $html=$html.$res[$i]["content"]."<br>";
+                        $html=$html."<a href='/final/index.php?comment=".$res[$i]["permalink"]."'> Permalink</a>". "<br>";
+                        $html=$html.TagsIdsToName($res[$i]["tags_ids"])."<br>";
+                        $html=$html."<br><br><br>";
+                 }
+
+                 echo $html;
+                ?>
+
+        </fieldset>
+        </form>
+     </div>
+  </dd>
+  <dd class="accordion-navigation">
+    <a href="#panel3b">Filter</a>
+    <div id="panel3b" class="content">
+     <!--TAB FILTER-->
+       
+
+            <!-- Start Create Comment Formular--------------------------- -->
+        <?php 
+
+           include "form_filter.php";
+
+        ?>
+        <!--------------------- End -->
+          
+       
+        </div> 
+       
+     
+        
+      
+     </div>
+  </dd>
+</dl>
+
+      
+      </strong>
+    
+    </div>
+
 
 
 
@@ -350,38 +468,19 @@
   </div>
 </footer>
 -->
-	<div class="large-12 columns" id="footer">
 
-	<ul class="breadcrumbs">
-	  <li class="current"><a href="#">Home</a></li>
-	  <li><a href="Benutzerhilfe.html">Benutzerhilfe</a></li>
-	  <li><a href="Impressum.html">Impressum</a></li>
-	  <li><a href="Code.html">Code</a></li>
-	</ul>
-	</div>
-	
-	<style>
-	#footer{
-		position:fixed;
-		bottom:0px;
-		
-	}
-	</style>
-
-
-
-  <!-- body content here -->
+  
 <!--Joyride Foundation Marike-->
 
 
 <!-- At the bottom of your page but inside of the body tag -->
 <ol class="joyride-list" data-joyride>
 	<li data-id="firstStop" data-text="Next" data-options="tip_location: bottom; nub_position: relative; prev_button: false">
-	<h5> Welcome ! </h5>
-    <p>Hello and welcome on Geocomment. On Login you can login or just click on the Map to start comment.</p>
+	 <h5> Welcome ! </h5>
+   <p>Hello and welcome on Geocomment. On Login you can login or just click on the Map to start comment.</p>
   </li>
 <li data-id="secondStop" data-text="Next" data-options="tip_location: top; " data-prev-text="Prev">
-	 <h6>New comment</h6>
+	  <h6>New comment</h6>
     <p>Here you can leave a new comment. Choose a position for your geodata on map.</p>
   </li>
   <li data-id="thirdStop" data-class="custom so-awesome" data-text="Next" data-prev-text="Prev">
@@ -408,15 +507,16 @@
 
 
 
-
+    <!-- Scripts to start foundation stuff -->
   <script src="js/vendor/jquery.js"></script>
   <script src="js/foundation/foundation.js"></script>
   <script src="js/foundation.min.js"></script>
   <script src="js/foundation/foundation.interchange.js"></script>
-  <script src="js/foundation/foundation.offcanvas.js"></script>
   <script src="js/foundation/foundation.joyride.js"></script>
   <script src="js/foundation/foundation.slider.js"></script>
   <script src="js/vendor/jquery.cookie.js"></script> <!-- Optional -->
+  <script src="js/foundation/foundation.magellan.js"></script>
+  <script src="js/foundation/foundation.accordion.js"></script>
   <script>
     $(document).foundation();
     
@@ -425,8 +525,26 @@
    
   </script>
 
+  <!--
 
-
+  <script>
+   
+    $(document).foundation({
+"magellan-expedition": {
+  active_class: 'active', // specify the class used for active sections
+  threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
+  destination_threshold: 5, // pixels from the top of destination for it to be considered active
+  throttle_delay: 20, // calculation throttling to increase framerate
+  fixed_top: 0, // top distance in pixels assigend to the fixed element on scroll
+  offset_by_height: true // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
+}
+});
+    
+   
+     $(document).foundation('magellan', 'reflow');
+   
+  </script>
+-->
 
 
 </body>
